6b66ec74be465c52c6322172495966641fff5f2d
Josema Calderón <49761941+josemacc@users.noreply.github.com>
2023-11-08 11:53:26 -0400

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -2,7 +2,7 @@
 
 $(git pull > a.txt)
 # Ejecutar git pull y filtrar la salida con grep
-if (tail -n 100 a.txt | grep -qi "change") || (tail -n 100 a.txt | grep -qi "cambio") ; then
+if (tail -n 100 a.txt | grep -qi "updating") || (tail -n 100 a.txt | grep -qi "actualizando") ; then
         # Ejecutar comandos adicionales
         echo "Actualizando..."
         # Fetched new data, unzip the file
